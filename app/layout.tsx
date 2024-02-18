f3374339bb6882a449b978b92a03262d3c1c1ec8
Extrieve <nicolas.cinera@gmail.com>
2024-02-18 11:55:39 -0500

    Update layout and page components

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={cn('bg-white text-slate-900 antialiased light', inter.className)}>
-      <body className={inter.className}>{children}</body>
+      <body className={'min-h-screen pt-12 bg-slate-50 antialiased'}>
+        {/* <Navbar /> */}
+        <div className="container max-w-7xl mx-auto h-full pt-12">
+        {children}
+        </div>
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='container align-middle'>
-      <h1 className='text-lg'>Welcome to GeoAchiever</h1>
-      <p>
+    <div className='m-auto text-center text-xl h-5'>
+      <h1>Welcome to GeoAchiever</h1>
+      <p className='text-sm'>
         An innovative application designed for explorers and achievers worldwide!
       </p>
       <Image
-        src="/images/geo-achiever-logo.png"
+        src="https://media.discordapp.net/attachments/862790378925850624/1208816617799159879/file-bF8nD5eM3ZxSCK8B17e7WJsC.png?ex=65e4a934&is=65d23434&hm=a4457b18fb8d79b0b9ff2c06a23f7438f2a320fba52895111ce8fba89c59aaa2&=&format=webp&quality=lossless"
         alt="GeoAchiever Logo"
         width={500}
         height={500}
